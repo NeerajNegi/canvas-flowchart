086b529f3d19df5fc7c46bf122fad9c6a49986b6
Neeraj Negi <neeraj.negi@SLP160139-Neeraj-417.local>
2019-10-22 16:02:56 +0530

    added dynamic scaling for circles

--- src/app/flowchart/flowchart.component.ts
+++ src/app/flowchart/flowchart.component.ts
@@ -11,41 +11,57 @@ export class FlowchartComponent implements OnInit {
 
   Flowchart: any;
   lineWidth: number = 100;
+  circleRadius: number = 100;
 
   DATA: any = {
     rectangles: [
       {
-        id: '18-676404',
+        id: 'R1-676404',
         connectionId: 'R1',
         midas: 'Midas #',
         outputs: 0,
         isDrawn: false,
         description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
         sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
-        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis 
-        nisl ut aliquip ex ea commodo consequat.`
+        Ut wisi enim ad minim veniam.`,
+        children: [
+          'ch1',
+          'ch2',
+          'ch3',
+          'ch4'
+        ]
       },
       {
-        id: '18-676404',
+        id: 'R2-676404',
         connectionId: 'R2',
         midas: 'Midas #',
         outputs: 0,
         isDrawn: false,
         description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
         sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
-        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis 
-        nisl ut aliquip ex ea commodo consequat.`
+        Ut wisi enim ad minim veniam.`,
+        children: [
+          'ch1',
+          'ch2',
+          'ch3',
+          'ch4'
+        ]
       },
       {
-        id: '18-676404',
+        id: 'R3-676404',
         connectionId: 'R3',
         midas: 'Midas #',
         outputs: 0,
         isDrawn: false,
         description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
         sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
-        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis 
-        nisl ut aliquip ex ea commodo consequat.`
+        Ut wisi enim ad minim veniam.`,
+        children: [
+          'ch1',
+          'ch2',
+          'ch3',
+          'ch4'
+        ]
       },
       {
         id: '18-676404',
@@ -55,8 +71,13 @@ export class FlowchartComponent implements OnInit {
         isDrawn: false,
         description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
         sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
-        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis 
-        nisl ut aliquip ex ea commodo consequat.`
+        Ut wisi enim ad minim veniam.`,
+        children: [
+          'ch1',
+          'ch2',
+          'ch3',
+          'ch4'
+        ]
       },
       {
         id: '18-676404',
@@ -66,8 +87,13 @@ export class FlowchartComponent implements OnInit {
         isDrawn: false,
         description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
         sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
-        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis 
-        nisl ut aliquip ex ea commodo consequat.`
+        Ut wisi enim ad minim veniam.`,
+        children: [
+          'ch1',
+          'ch2',
+          'ch3',
+          'ch4'
+        ]
       }
     ],
     circles: [
@@ -99,10 +125,11 @@ export class FlowchartComponent implements OnInit {
     connections: [
       'R1-1:C1-1',
       'R1-2:C2-1',
+      'R1-3:C4-1',
       'C1-1:R2-1',
       'R2-1:C3-1',
-      'C3-1:R4-1',
-      'R4-1:C4-1'
+      'C3-1:R3-1',
+      
       // 'C2-1:R3-1',
       // 'R3-1:C3-1',
       // 'C3-1:R4-1',
@@ -127,12 +154,14 @@ export class FlowchartComponent implements OnInit {
       const [ from, fromPort ] = connection.split(':')[0].split('-');
       const [ to, toPort ] = connection.split(':')[1].split('-');
 
+      //Draw From Node
       if(from[0] === 'R') {
         const rect = this.DATA.rectangles.find(rectangle => rectangle.connectionId === from);
         if(!rect.isDrawn) {
           rect.isDrawn = true;
           if(index === 0) {
-            this.addRectangle(this.Flowchart, null, from);
+            const rectWrapper = this.addRectangle(this.Flowchart, null, from);
+            this.appendRectangleData(rect, rectWrapper);
           } 
           // guess is that 'from' will only be created once in the beginning.
           // Afterwards the 'to' node will become 'from' node eventually (Not sure).
@@ -144,10 +173,12 @@ export class FlowchartComponent implements OnInit {
         }
       }
 
+      //Draw To Node
       if(to[0] === 'R') {
         const inputNode = d3.select(`#${from}`);
-        this.addRectangle(this.Flowchart, inputNode, to);
-
+        const rect = this.DATA.rectangles.find(rectangle => rectangle.connectionId === to);
+        const rectWrapper = this.addRectangle(this.Flowchart, inputNode, to);
+        this.appendRectangleData(rect, rectWrapper);
       } else if(to[0] === 'C') {
         const inputNode = d3.select(`#${from}`);
         const rect = this.DATA.rectangles.find(rectangle => rectangle.connectionId === from);
@@ -159,6 +190,34 @@ export class FlowchartComponent implements OnInit {
       }
 
       this.addLine(this.Flowchart, d3.select(`#${from}`), d3.select(`#${to}`));
+    });
+  }
+
+  private appendRectangleData(rect, node): void {
+    const header = node.append('h2').text(() => rect.id);
+    const midasNumber = node.append('h4').text(() => rect.midas);
+    const description = node.append('p').text(() => rect.description);
+    node.on('click', () => {
+      const nodeDomRect = node.node().getBoundingClientRect();
+      console.log(nodeDomRect);
+      if(d3.select(`#${rect.id}`).empty()) {
+        
+        const top = nodeDomRect.top - 20;
+        const left = nodeDomRect.left + nodeDomRect.width - 20;
+        let childrenList = this.Flowchart
+          .append('div')
+          .attr('class','children-list')
+          .attr('id', rect.id)
+          .style('top', top + 'px')
+          .style('left', left + 'px');
+        
+        childrenList
+          .selectAll('p')
+          .data(rect.children)
+          .enter()
+          .append('p')
+          .text((d) => d)
+      }
     });
   }
 
@@ -201,15 +260,18 @@ export class FlowchartComponent implements OnInit {
   }
 
   private addCircle(parent, neighbour, id='', fromPort, totalPorts): any {
+      const circleRadius = this.circleRadius / totalPorts;
       const domRect = neighbour.node().getBoundingClientRect();
       // const top = domRect.top + domRect.height / 4;
-      const top = domRect.top + (fromPort * 2 - 1)*(domRect.height / (totalPorts * 2)) - 50;
+      const top = domRect.top + (fromPort * 2 - 1)*(domRect.height / (totalPorts * 2)) - circleRadius / 2;
       const left = domRect.left + domRect.width + this.lineWidth;
       return parent
         .append('div')
         .attr('class', 'circle')
         .attr('id',id)
         .attr('type','circle')
+        .style('width', circleRadius + 'px')
+        .style('height', circleRadius + 'px')
         .style('top', top + 'px')
         .style('left', left + 'px')
   }
